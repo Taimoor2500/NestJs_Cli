f9a8dfe3ea2a7f24cc4336c30419cbe3754735d8
Taimoor Rashid <taimoor.rashid@dubizzlelabs.com>
2025-04-13 20:19:44 +0500

    feat: add message handling functionality and update MessagesController methods

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -4,20 +4,25 @@ import { MessagesService } from './messages.service';
 
 @Controller('messages') //class decorators
 export class MessagesController {
+
+    messagesService: MessagesService;
+
+    constructor() {
+        this.messagesService = new MessagesService();
+    }
+
     @Get() // method decorators
     listMessages() {
-
+        return this.messagesService.findAll();
     }
 
     @Post()
     createMessage(@Body() body: CreateMessageDto ) {
-        console.log(body);
-
+        return this.messagesService.create(body.content);
     }
 
     @Get('/:id')
     getMessage(@Param('id') id: string) {
-        console.log(id);
-
+        return this.messagesService.findOne(id);
     }
 }
